57c67ad55495e461688fff2c38dbd04e854a5bd2
avoevodskiy <vavnf@mail.ru>
2018-02-12 00:15:02 +0400

    Вторая попытка сихронизации

---
+++ UCServer/Models/UCdbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace UCServer.Models
+{
+    public class UCdbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; } 
+        public DbSet<City> Cities { get; set; }
+        public DbSet<UserCity> UsersCities { get; set; }
+        //конструктор
+        public UCdbContext(DbContextOptions<UCdbContext> options) : base(options)
+        {
+        }
+    }
+}
--- UCServer/Startup.cs
+++ UCServer/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using UCServer.Models;
+
 
 namespace UCServer
 {
@@ -24,6 +27,8 @@ namespace UCServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //региструем контекст бд, строка подключения берется из appsetting.json
+            services.AddDbContext<UCdbContext>(options =>options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
